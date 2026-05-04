9974c4e160a12af5e5642f57ce36b4d88e217f7c
REZAULKARIM2024 <rknyc2021@gmail.com>
2026-05-03 22:33:46 -0400

    Refactor ExcelUtils to improve getTestData method
    Refactored getTestData method to use InputStream and XSSFWorkbook for better resource handling.

--- src/test/java/utils/ExcelUtils.java
+++ src/test/java/utils/ExcelUtils.java
@@ -1,25 +1,32 @@
 package utils;
 
+import java.io.InputStream;
 import org.apache.poi.ss.usermodel.*;
-import java.io.FileInputStream;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
-
-    public static Object[][] getTestData(String path, String sheetName) throws Exception {
-        FileInputStream fis = new FileInputStream(path);
-        Workbook wb = WorkbookFactory.create(fis);
-        Sheet sheet = wb.getSheet(sheetName);
-
-        int rows = sheet.getPhysicalNumberOfRows();
-        int cols = sheet.getRow(0).getLastCellNum();
-
-        Object[][] data = new Object[rows - 1][cols];
-
-        for (int i = 1; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                data[i - 1][j] = sheet.getRow(i).getCell(j).toString();
+    public static Object[][] getTestData(String fileName, String sheetName) {
+        try {
+            InputStream is = ExcelUtils.class.getClassLoader().getResourceAsStream(fileName);
+            if (is == null) {
+                throw new RuntimeException("Resource not found: " + fileName);
             }
+            Workbook workbook = new XSSFWorkbook(is);
+            Sheet sheet = workbook.getSheet(sheetName);
+            int rowCount = sheet.getPhysicalNumberOfRows();
+            int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+            Object[][] data = new Object[rowCount - 1][colCount];
+            for (int i = 1; i < rowCount; i++) {
+                Row row = sheet.getRow(i);
+                for (int j = 0; j < colCount; j++) {
+                    Cell cell = row.getCell(j);
+                    data[i - 1][j] = cell == null ? "" : cell.toString();
+                }
+            }
+            workbook.close();
+            return data;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return data;
     }
 }
